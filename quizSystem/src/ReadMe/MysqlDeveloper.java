8eb0479b6f23a128ead317a4c9d1bc8fc5f46af4
Kaiyuan Ma <84887870+KaiyuanMa@users.noreply.github.com>
2021-09-24 16:46:07 -0400

    Update MysqlDeveloper.java

--- quizSystem/src/ReadMe/MysqlDeveloper.java
+++ quizSystem/src/ReadMe/MysqlDeveloper.java
@@ -1,8 +1,8 @@
 package ReadMe;
 
 public class MysqlDeveloper {
-    private String user = "root";
-    private String password = "147qwe147";
+    private String user = "";
+    private String password = "";
     private String hostname = "3306";
 
     public String getHostname() {
